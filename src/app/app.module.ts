00b8a1702b0be6b2760888a46e9c25e6080b0a91
Ibraim <ibraim99.5@gmail.com>
2024-10-22 07:50:56 -0300

    New screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { CurrentWlComponent } from './current-wl/current-wl.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    CurrentWlComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/current-wl/current-wl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-wl',
+  templateUrl: './current-wl.component.html',
+  styleUrl: './current-wl.component.css'
+})
+export class CurrentWlComponent {
+
+}
